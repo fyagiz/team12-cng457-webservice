3cf89ec29339fb25523ca296d08a11d2a3a41401
idil.ece.trabzon <ece.trabzon@metu.edu.tr>
2021-05-13 15:10:09 +0300

    Product-Computer-Phone add

---
+++ src/main/java/com/team12/team12/entity/Product.java
@@ -0,0 +1,28 @@
+package com.team12.team12.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private int productID;
+    private float screenSize;
+    private float price;
+    private String model;
+    @ManyToOne
+    private Brand brand;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<CommentAndRating> commentAndRatingList;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<AdditionalFeature> additionalFeatureList;
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+    private Phone phone;
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+    private Computer computer;
+}
