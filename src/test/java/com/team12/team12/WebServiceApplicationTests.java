5dbcc0bbdcc7b556ab6d1b7ef205c1d89610bb04
fyagiz <furkanyagiz@gmail.com>
2021-05-13 13:25:16 +0300

    Spring initializr

---
+++ src/test/java/com/team12/team12/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team12.team12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
