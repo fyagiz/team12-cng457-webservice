6a53e04bee39b5d4a5eed21bb08c71cbd7860840
gizemunder <e201626@metu.edu.tr>
2021-06-05 20:04:19 +0300

    Create AdditionalFeatureRepository.java
    Additional feature is created for communicate with database.

---
+++ src/main/java/com/team12/repository/AdditionalFeatureRepository.java
@@ -0,0 +1,7 @@
+package com.team12.repository;
+
+import com.team12.entity.AdditionalFeature;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdditionalFeatureRepository extends JpaRepository<AdditionalFeature,Integer> {
+}
